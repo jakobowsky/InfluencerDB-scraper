81eaa5b195778925401d9d7376b460f1d193ec62
jakobowsky <jakob.wolitzki@gmail.com>
2020-01-25 17:56:21 +0100

    Rebuild scrapers functions

--- scrap.py
+++ scrap.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 class InstagramScraper(object):
 
-    def __init__(self, **kwargs):
+    def __init__(self):
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
                           'AppleWebKit/537.11 (KHTML, like Gecko) '
@@ -42,39 +42,39 @@ class InstagramScraper(object):
             'window._sharedData =', '').replace(';', '')
         return json.loads(raw_string)
 
-    def profile_page_metrics(self, link):
-        # nice function but not needed for now
-        results = {}
+    def get_current_profile_info(self, username):
         try:
+            link = f"https://www.instagram.com/{username}/"
             response = self.__request_url(link)
             json_data = self.extract_json_data(response)
-            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']
         except Exception as e:
             raise e
         else:
-            for key, value in metrics.items():
-                if key != 'edge_owner_to_timeline_media':
-                    if value and isinstance(value, dict):
-                        value = value['count']
-                        results[key] = value
-                    elif value:
-                        results[key] = value
+            profile_page_metrics = self.profile_page_metrics(json_data)
+            profile_page_recent_posts = self.profile_page_recent_posts(json_data)
+        # return info and add it to api in another function
+
+
+    def profile_page_metrics(self, json_data_from_profile):
+        results = {}
+        metrics = json_data_from_profile['entry_data']['ProfilePage'][0]['graphql']['user']
+        for key, value in metrics.items():
+            if key != 'edge_owner_to_timeline_media':
+                if value and isinstance(value, dict):
+                    value = value['count']
+                    results[key] = value
+                elif value:
+                    results[key] = value
         return results
 
-    def profile_page_recent_posts(self, link):
+    def profile_page_recent_posts(self, json_data_from_profile):
         results = []
-        try:
-            response = self.__request_url(link)
-            json_data = self.extract_json_data(response)
-            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media'][
-                "edges"]
-        except Exception as e:
-            raise e
-        else:
-            for node in metrics:
-                node = node.get('node')
-                if node and isinstance(node, dict):
-                    results.append(node)
+        metrics = json_data_from_profile['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media'][
+            "edges"]
+        for node in metrics:
+            node = node.get('node')
+            if node and isinstance(node, dict):
+                results.append(node)
         return results
 
     def discover_posts(self, hashtag):
@@ -95,15 +95,19 @@ class InstagramScraper(object):
         return results
 
     def get_account_name_from_post(self, post_id):
-        response = self.__request_url(f"https://www.instagram.com/p/{post_id}/")
-        json_data = self.extract_json_data(response)
-        import pdb
-        pdb.set_trace()
-        # metrics = json_data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_top_posts'][
-        #     "edges"]
+        try:
+            response = self.__request_url(f"https://www.instagram.com/p/{post_id}/")
+            json_data = self.extract_json_data(response)
+            username = json_data['entry_data']['PostPage'][0]['graphql']['shortcode_media']['owner']['username']
+            return username
+        except Exception as e:
+            raise e
+
+
+class ExploreInstragram():
+
+    def __init__(self):
+        scraper = InstagramScraper()
 
 x = InstagramScraper()
-m1 = x.discover_posts('codingmemes')
-print(m1)
-# m1 = x.profile_page_metrics('https://www.instagram.com/jakobowsky/')
-# m2 = x.profile_page_metrics('https://www.instagram.com/codingdays/')
+m1 = x.get_account_name_from_post('B7v3XnQgz5x')
